14373e1a04b4980a852a50b68db95d9740593444
Akash <akashraturi33@gmail.com>
2021-04-05 19:44:00 +0530

    initial

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -38,6 +38,21 @@ export default class CreateRoomPage extends Component {
                   </RadioGroup>
               </FormControl>
             </Grid>
+            <Grid item xs={12} align="center">
+                <FormControl>
+                    <TextField required={true} type="number" defaultValue={this.defaultVotes} inputProps={{min: 1, style: {textAlign: "center"}}} />
+                    <FormHelperText>
+                        <div align="center">Votes required to Skip song</div>
+                    </FormHelperText>
+                </FormControl>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Button color="primary" variant="contained">Create A Room</Button>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Button color="secondary" variant="contained" to="/" component={Link}>Create A Room</Button>
+                Back
+            </Grid>
         </Grid>
         );
     }
