fa03eea103d6df3e22dce2a89e3c2be7b51fe93e
Akash <akashraturi33@gmail.com>
2021-04-09 19:17:03 +0530

    Room join page

--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -8,7 +8,9 @@ export default class RoomJoinPage extends Component {
         this.state = {
             roomCode: "",
             error: ""
-        }
+        };
+        this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
+        this.roomButtonPressed = this.roomButtonPressed.bind(this);
     }
 
     render() {
@@ -26,11 +28,12 @@ export default class RoomJoinPage extends Component {
                         placeholder="Enter a Room Code"
                         value={this.state.roomCode}
                         helperText={this.state.error}
-                        variant="outlined" 
+                        variant="outlined"
+                        onChange={this.handleTextFieldChange} 
                     />
                 </Grid>
                 <Grid item xs={12} align="center">
-                <Button variant="contained" color="primary" onClick>
+                <Button variant="contained" color="primary" onClick={this.roomButtonPressed}>
                     Enter Room
                 </Button>
                 </Grid>
@@ -41,5 +44,31 @@ export default class RoomJoinPage extends Component {
                 </Grid>
             </Grid>
         );
+    }
+
+    handleTextFieldChange(e) {
+        this.setState({
+            roomCode: e.target.value
+        });
+    }
+
+    roomButtonPressed() {
+        const requestOptions = {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                code: this.state.roomCode,
+            }),
+        };
+        fetch('/api/join-room', requestOptions).then((response) => {
+            if(response.ok) {
+                this.props.history.push(`/room/${this.state.roomCode}`);
+            } else {
+                this.setState({error: "Room not found."});
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
     }
 }
